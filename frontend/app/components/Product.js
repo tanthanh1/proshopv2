d7a1ad07f0dc3204393125c5af7e0551b24fee26
Thanh <lygiathanh@gmail.com>
2023-08-15 22:53:51 +0700

    deloy

--- frontend/app/components/Product.js
+++ frontend/app/components/Product.js
@@ -5,18 +5,20 @@ import Rating from "./Rating";
 const Product = ({ product }) => {
     return (
         <>
-            <div className="border border-solid  rounded border-[rgba(0, 0, 0, 0.176)] shadow p-5">
+            <div className="flex flex-col  items-center gap-4 border border-solid  rounded border-[rgba(0, 0, 0, 0.176)] shadow p-5">
                 <Link
-                    className="text-center mt-2 block font-bold"
+                    className="text-center mt-2 block font-bold flex-1"
                     href={`/product/${product._id}`}
                 >
                     <img className="mb-4" src={product.image} alt="" />
                     {product.name}
                 </Link>
-                <p>
-                    <Rating score={product.rating} />{" "}
-                </p>
-                <p className="text-center mt-2 font-medium text-[24px] text-[#7b8a8b]">
+
+                <div className="text-red-700 ">
+                    <Rating score={product.rating} />
+                </div>
+
+                <p className="text-center  font-medium text-[24px] text-[#7b8a8b]">
                     $ {product.price}
                 </p>
             </div>
